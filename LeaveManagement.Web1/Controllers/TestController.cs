4f992cb1c657eabb26ad63ffb1e03560fb7481f8
StrongholdPC <StrongholdPC@DESKTOP-7N9FQB3>
2025-03-19 22:31:29 +0300

    Add project files.

---
+++ LeaveManagement.Web1/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagement.Web1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagement.Web1.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Bob The student",
+                DateOfBirth = new DateTime(1998, 12, 16)
+            };
+            return View(data);
+        }
+    }
+}
---
+++ LeaveManagement.Web1/Models/TestViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LeaveManagement.Web1.Models
+{
+    public class TestViewModel
+    {
+
+        public string Name { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
